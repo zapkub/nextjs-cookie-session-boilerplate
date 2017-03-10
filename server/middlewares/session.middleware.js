224954dd96b67dc51375132fc6ea9a8e1501915e
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2017-03-10 19:52:09 +0700

    เพิ่ม mongoose User model ให้ตรวจพาสเวิร์ดได้, เช็คพาสเวิร์ดที่ middleware login

--- server/middlewares/session.middleware.js
+++ server/middlewares/session.middleware.js
@@ -1,34 +1,36 @@
 const mockUser = {
-    email: 'Test',
-}
-exports.deserializeSession = (context) => {
-    return async function (id, done) {
-        context.logger.log('Deserialize');
-        context.logger.log(id);
+  email: 'Test',
+};
+exports.deserializeSession = context => async function (id, done) {
+  context.logger.log('Deserialize');
+  context.logger.log(id);
 
-        const user = await context.User.findById(id);
-        if(user) {
-            done(null, user.toObject());
-        } else {
-            done('error')
-        }
-    }
-}
+  const user = await context.User.findById(id);
+  if (user) {
+    done(null, user.toObject());
+  } else {
+    done('error');
+  }
+};
 
 
-exports.LocalStrategyHandler = (context) => async (email, password, done) => {
-    console.log('login');
-    const user = await context.User.findOne({email,});
-    if(user) {
-         done(null , user);
+exports.LocalStrategyHandler = context => async (email, password, done) => {
+  console.log('login');
+  const user = await context.User.findOne({ email });
+  if (user) {
+    const isValidPassword = (new context.User()).comparePassword();
+    if (isValidPassword) {
+      done(null, user);
     } else {
-         done('Error');
+      done(null, user);
     }
-   
-}
+  } else {
+    done('Error');
+  }
+};
 
-exports.serializeSession = (context) => (user, done) => {
-    console.log('Serialize session');
-    done(null, user._id);
-}
+exports.serializeSession = context => (user, done) => {
+  console.log('Serialize session');
+  done(null, user._id);
+};
 
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -3,20 +3,24 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-    },
-    password: String
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
 }, {
-        timestamps: true,
-    });
+  timestamps: true,
+});
 
-userSchema.methods.createUser = async function(Args) {
-    const user = new User(Args);
-    await user.save();
-    return user;
-}
+userSchema.methods.createUser = async function (Args) {
+  const user = new User(Args);
+  await user.save();
+  return user;
+};
+
+userSchema.methods.comparePassword = function (password) {
+  return true;
+};
 
 const User = mongoose.model('User', userSchema);
 
